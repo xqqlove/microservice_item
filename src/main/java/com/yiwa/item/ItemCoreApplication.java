3f17c0226130e59fc4960b0e52c91cec75d15f95
Administrator <1977579350@qq.com>
2020-05-11 23:29:31 +0800

    Initial commit

---
+++ src/main/java/com/yiwa/item/ItemCoreApplication.java
@@ -0,0 +1,11 @@
+package com.yiwa.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemCoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ItemCoreApplication.class, args);
+    }
+}
